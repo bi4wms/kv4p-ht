dc9a10c4fbbb2dd5d92bf42ad7f775b1fd98a315
bi4wms <68187708+bi4wms@users.noreply.github.com>
2026-03-25 10:26:21 +0800

    Modify ESP32_PRODUCT_IDS for USB device matching
    Updated ESP32_PRODUCT_IDS to change the second product ID from 29987 to 29986.

--- android-src/KV4PHT/app/src/main/java/com/vagell/kv4pht/radio/RadioAudioService.java
+++ android-src/KV4PHT/app/src/main/java/com/vagell/kv4pht/radio/RadioAudioService.java
@@ -120,7 +120,7 @@ public class RadioAudioService extends Service implements PacketHandler {
 
     // === USB Device Matching ===
     private static final int[] ESP32_VENDOR_IDS = {4292, 6790};
-    private static final int[] ESP32_PRODUCT_IDS = {60000, 29987};
+    private static final int[] ESP32_PRODUCT_IDS = {60000, 29986}; //bi4wms change from 29987 to 29986
 
     // === Audio Constants ===
     public static final int AUDIO_SAMPLE_RATE = 48000;
